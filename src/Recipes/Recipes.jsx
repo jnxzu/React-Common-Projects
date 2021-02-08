dfd236f610a3d58651c2c1cf8f86ae29c76298f0
J <janbielowka@gmail.com>
2021-02-08 14:23:54 +0100

    recipes tweaks

--- src/Recipes/Recipes.jsx
+++ src/Recipes/Recipes.jsx
@@ -1,59 +1,338 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import _ from 'lodash';
+import React from 'react';
 
 import './Recipes.scss';
 
 function Recipes() {
-  const [ready, setReady] = useState(false);
-  const [recipes, setRecipes] = useState([]);
-
-  const getRecipes = () => {
-    const options = {
-      method: 'GET',
-      url: 'https://tasty.p.rapidapi.com/recipes/list',
-      params: {
-        size: 10,
-        from: _.random(1111),
-      },
-      headers: {
-        'x-rapidapi-key': process.env.REACT_APP_TASTY_API_KEY,
-        'x-rapidapi-host': 'tasty.p.rapidapi.com',
-      },
-    };
-
-    axios.request(options).then((res) => {
-      setRecipes(res.data.results);
-      setReady(true);
-    });
-  };
-
-  useEffect(() => {
-    getRecipes();
-  }, []);
-
   return (
     <div id="recipes" className="recipes">
-      {ready ? (
-        <div className="recipes__container">
-          {recipes.map((recipe) => (
-            <a
-              key={recipe.id}
-              href={recipe.original_video_url || recipe.thumbnail_url}
-              className="recipe"
-            >
-              <div className="recipe__name">
-                <h1>{recipe.name}</h1>
-              </div>
-              <div className="recipe__image">
-                <img alt={recipe.name} src={recipe.thumbnail_url} />
-              </div>
-            </a>
-          ))}
+      <div className="recipe">
+        <h1>Curtis Stone&apos;s Aussie Burger</h1>
+        <div>
+          <ul>
+            <li>4 x 170 g beef burger patties</li>
+            <li>4 slices tasty cheese</li>
+            <li>4 hamburger buns</li>
+            <li>20 g baby rocket</li>
+            <li>4 fried eggs</li>
+            <li>1 splash extra virgin olive oil *for brushing</li>
+            <li>1 pinch salt and pepper *to taste</li>
+          </ul>
+          <p>
+            Heat barbecue to high. Season burger patties with salt and pepper.
+            Brush barbecue grates with oil and add patties. Barbecue patties for
+            2 minutes or until bottoms are lightly charred. Flip patties and top
+            with cheese. Barbecue for a further 2 minutes, or until bottoms are
+            lightly charred and cheese has melted. Set patties aside on plate.
+            Meanwhile, barbecue cut sides of buns for 1 minute, or until lightly
+            toasted. Spread chilli mayo over bottom halves of buns. Top with
+            rocket, burger patties, beetroot relish and eggs. Top with bun tops
+            and serve.
+          </p>
+          <img
+            src="https://img.bestrecipes.com.au/E-e9f3mw/w643-h428-cfill-q90/br/2019/01/curtis-stones-aussie-burger-recipe-524088-1.jpg"
+            alt="Curtis Stone's Aussie Burger"
+          />
         </div>
-      ) : (
-        <img src="recipe_loading.gif" alt="loading" />
-      )}
+      </div>
+      <div className="recipe">
+        <h1>Bacon &amp; Spinach Stuffed Chicken</h1>
+        <div>
+          <ul>
+            <li>4 boneless skinless chicken breasts</li>
+            <li>Kosher salt</li>
+            <li>Freshly ground black pepper 4 oz.</li>
+            <li>cream cheese, softened 1/2 c.</li>
+            <li>frozen spinach, defrosted and drained 1/3 c.</li>
+            <li>chopped artichoke hearts 1 c.</li>
+            <li>shredded mozzarella, divided</li>
+            <li>Pinch crushed red pepper flakes 4</li>
+            <li>strips bacon, cut into 4 strips 2 tbsp.</li>
+            <li>extra-virgin olive oil</li>
+          </ul>
+          <p>
+            Preheat oven to 400°. Line a large baking sheet with foil. Make
+            slits widthwise in chicken, being careful not to cut all the way
+            through chicken. Season with salt and pepper. Place on prepared
+            baking sheet. In a medium bowl, combine cream cheese, spinach,
+            artichokes, and ½ cup of mozzarella. Season with salt, pepper, and a
+            pinch of red pepper flakes. Fill every other slit with cream cheese
+            mixture and fill remaining slits with a piece of bacon. Sprinkle
+            remaining ½ cup mozzarella on top and drizzle with oil. Bake until
+            chicken is cooked through and bacon is crispy, 35 minutes.
+          </p>
+          <img
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/07-bacon-spinach-stuffed-chicken-11866-v-1582648479.jpg?crop=1.00xw:0.667xh;0,0.0238xh&resize=768:*"
+            alt="Bacon And Spinach Stuffed Chicken"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Vegan Cobb Salad with Coconut “Bacon”</h1>
+        <div>
+          <ul>
+            <li>1 small head romaine lettuce, chopped</li>
+            <li>½ cup halved cherry tomatoes</li>
+            <li>1 small cucumber, thinly sliced</li>
+            <li>1 medium zucchini, spiralized or peeled into ribbons</li>
+            <li>1 ear of corn, grilled, kernels sliced off cobb</li>
+            <li>1 avocado, pitted and diced</li>
+            <li>12 to 14 ounces extra-firm tofu, patted dry and cubed</li>
+            <li>½ cup coconut “bacon”</li>
+          </ul>
+          <p>
+            In a blender, combine the cashews, ½ cup water, lemon juice, onion
+            powder, garlic powder and ¼ teaspoon of sea salt. Blend until
+            creamy, adding more water if necessary. Taste and season with
+            additional salt as needed. Chill until ready to use. Preheat the
+            oven to 350°F and line a baking sheet with parchment paper. Place
+            the coconut flakes along with the tamari, maple syrup and smoked
+            paprika on the pan and toss gently to coat. Spread in a thin layer
+            on the pan and bake until dark golden brown and slightly crispy,
+            about 10 minutes. Increase the oven temperature to 400°F and line a
+            baking sheet with parchment paper. Place the tofu cubes on the pan
+            and toss with a drizzle of olive oil and generous pinches of salt.
+            Bake for 15 to 17 minutes or until golden brown around the edges. In
+            a serving bowl, assemble the salad with the romaine, cherry
+            tomatoes, cucumber, zucchini, corn, avocado and tofu. Top with the
+            coconut “bacon” and serve with the dressing on the side. Store any
+            extra dressing in a sealed container in the fridge.
+          </p>
+          <img
+            src="https://cdn.loveandlemons.com/wp-content/uploads/2016/06/IMG_2016_05_14_07850-cropped.jpg"
+            alt='Vegan Cobb Salad with Coconut "Bacon"'
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>&quot;Fried&quot; Ice Cream</h1>
+        <div>
+          <ul>
+            <li>
+              1 pint ice cream (such as Breyers Delights Vanilla Cupcake Ice
+              Cream)
+            </li>
+            <li>1 1/2 tbsp. butter</li> <li>1 1/4 c. corn cereal, crushed</li>{' '}
+            <li>1/2 tsp. ground cinnamon</li> <li>2 tsp. granulated sugar</li>{' '}
+            <li>Whipped cream, for garnish</li>
+            <li>Sprinkles, for garnish</li>{' '}
+            <li>4 maraschino cherries, for garnish</li>
+          </ul>
+          <p>
+            Line a small sheet tray with parchment paper and place in freezer.
+            Scoop ice cream into 4 balls and place on prepared sheet tray. Keep
+            in freezer while making cereal mixture, at least 30 minutes. In a
+            medium skillet over medium heat, melt butter. Add corn cereal and
+            cinnamon and cook, stirring occasionally, until the cereal turns
+            golden, 5 to 7 minutes. Remove from heat and stir in sugar. Place on
+            a shallow bowl and let cool. Remove ice cream balls from freezer and
+            roll in cereal mixture. Top with whipped cream, sprinkles, and a
+            cherry, and serve immediately.
+          </p>
+          <img
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-friedicecream-072-1587757338.jpg?crop=1.00xw:0.801xh;0,0.0585xh&resize=768:*"
+            alt='"Fried" Ice Cream'
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Chargrilled Chicken Wings Recipe</h1>
+        <div>
+          <ul>
+            <li>4 lb. Chicken Winglets</li>
+            <li>1 ½ tbsp. Kosher salt</li>
+            <li>1 tbsp. Coarse black pepper</li>
+            <li>½ tbsp. Seasoned Salt</li>
+            <li>1 ½ tsp. Smoked Paprika</li>
+            <li>1 ½ tsp. Onion powder</li>
+            <li>1 ½ tsp. Garlic powder</li>
+            <li>1 tsp. Chilli powder</li>
+            <li>1 tsp. Italian Seasoning</li>
+            <li>1 tsp. Thyme</li>
+            <li>1 tsp. Oregano leaves</li>
+            <li>½ lemon</li>
+            <li>1 ½ tbsp. Worcestershire Sauce</li>
+            <li>1 tsp. Liquid Smoke</li>
+          </ul>
+          <p>
+            In a large bowl, add the chicken. Begin to add the seasonings to the
+            chicken (salt, pepper, seasoned salt, smoked paprika, onion powder,
+            garlic powder, chili powder, Italian seasoning, Thyme, oregano
+            leaves, squeeze the lemon, Worcestershire Sauce, and liquid smoke.)
+            Use tongs or cover hands with disposable gloves and toss chicken in
+            seasonings. Cover with plastic wrap and place in refrigerator for,
+            at least, 30 minutes. Prepare your grill as desired. However, place
+            most of the charcoal more heavily on one side of the bottom of the
+            grill. Proceed to start fire according to manufacturer instructions.
+            Cover half of the grill with aluminum foil. Once it’s safe, place
+            the grill on top of the coals. Place the wings, with tongs, on the
+            coal-heavy side of the grill. Flip the chicken wings after 5 minutes
+            on each side. Place the chicken on the aluminum foil side to finish
+            cooking. Remove from heat once chicken reaches an internal
+            temperature of 165. Remove from heat and cover with aluminum foil
+            until ready to serve.
+          </p>
+          <img
+            src="https://i1.wp.com/bsugarmama.com/wp-content/uploads/2017/04/chargrilled-chicken-wings-1.jpg?resize=680%2C400&ssl=1"
+            alt="Chargrilled Chicken Wings Recipe"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Vegan Tacos with Smoky Chipotle Portobellos</h1>
+        <div>
+          <ul>
+            <li>2 extra large portobello mushrooms</li>
+            <li>1 red bell pepper</li>
+            <li>½ an onion – optional</li>
+            <li>1 tablespoon oil</li>
+            <li>2 tablespoons canned Chipotle in Adobo sauce ( SAUCE ONLY)</li>
+            <li>1 minced garlic clove ( or ½ teaspoon granulated garlic)</li>
+            <li>½ teaspoon cumin</li>
+            <li>½ teaspoon coriander</li>
+            <li>salt to taste</li>
+            <li>4 tortillas, warmed</li>
+            <li>1 can refried black beans, warmed</li>
+          </ul>
+          <p>
+            Preheat oven to 425F Slice the portobellos into ½ inch thick wedges
+            and slice bell pepper in to ½ thick strips. If adding onion, cut
+            into ½ inch thick rings or half moons. Place all on a sheet-pan
+            lined sheet pan &amp; Mix marinade ingredients together in a small
+            bowl. Brush both sides of mushrooms liberally with the marinade,
+            then remaining red bell pepper and onion lightly. Sprinkle
+            portobellos with salt. Roast 20 minutes or until portobellos are
+            fork-tender. While this is roasting, heat the beans any prep any
+            additional garnishes. Pickled onions and Vegan cilantro Crema both
+            take about 10 minutes to make. Or simply use avocado slices. When
+            ready to serve, warm the tortillas ( over a gas flame on the stove,
+            or in a toaster oven) and spread generously with the refried black
+            beans. Divide chipotle portobellos and peppers (and onions if
+            used)among the tortillas. Top with Cilantro Crema, Poblano Salsa, or
+            avocado, fresh cilantro and optional pickled onions.
+          </p>
+          <img
+            src="https://www.feastingathome.com/wp-content/uploads/2018/03/Sheet-Pan-Portobello-Tacos-207.jpg"
+            alt="Vegan Tacos with Smoky Chipotle Portobellos"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Cilantro-Lime Shrimp Pasta</h1>
+        <div>
+          <ul>
+            <li>1/4 c. extra-virgin olive oil</li>
+            <li>2 tbsp. lime juice</li>
+            <li>1 tsp. ground cumin</li>
+            <li>1 lb. shrimp, peeled and deveined</li>
+            <li>3/4 lb. spaghetti</li>
+            <li>4 tbsp. butter, divided</li>
+            <li>3 cloves garlic, minced</li>
+            <li>1/2 tsp. red pepper flakes</li>
+            <li>1/4 c. low-sodium chicken broth</li>
+            <li>1/4 c. white wine</li>
+            <li>1/4 c. freshly chopped cilantro</li>
+            <li>Lime slices, for serving</li>
+          </ul>
+          <p>
+            In a medium bowl, whisk together olive oil, lime juice, and cumin.
+            Add shrimp and toss to combine. Cover and refrigerate for 15 minutes
+            or up to 4 hours. Before cooking shrimp, boil pasta: In a large pot
+            of boiling salted water, cook pasta according to package
+            instructions until al dente. Drain. Cook shrimp: In a large skillet
+            over medium heat, heat 2 tablespoons butter. Add shrimp in a single
+            layer and cook until pink and opaque, about 2 minutes per side.
+            Remove shrimp from skillet. Add remaining 2 tablespoons butter to
+            skillet. When melted, stir in garlic, and red pepper flakes and cook
+            until fragrant, about 1 minute. Add chicken broth and white wine,
+            and bring mixture to a simmer. Add cooked pasta and return shrimp to
+            skillet. Toss everything together until evenly coated in sauce.
+            Serve warm with cilantro and lime slices.
+          </p>
+          <img
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-cilantro-lime-shrimp-pasta-pinterest-still001-1522692936.jpg?crop=1xw:1xh;center,top&resize=980:*"
+            alt="Cilantro-Lime Shrimp Pasta"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Whiskey Sour</h1>
+        <div>
+          <ul>
+            <li>2 ounces bourbon</li>
+            <li>3/4 ounce lemon juice, freshly squeezed</li>
+            <li>1/2 ounce simple syrup</li>
+            <li>1/2 ounce egg white (optional)</li>
+            <li>Garnish: Angostura bitters</li>
+          </ul>
+          <p>
+            Add bourbon, lemon juice, simple syrup and egg white, if using, to a
+            shaker and dry-shake for 30 seconds without ice. Add ice and shake
+            again until well-chilled. Strain into a coupe glass. Garnish with 3
+            or 4 drops of Angostura bitters.{' '}
+          </p>
+          <img
+            src="https://www.liquor.com/thmb/XCAivtl0pyQdDaDr8jFq-wtGI5Q=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/whiskey-sour-720x720-primary-v2-4fc831b613964da5a19cdbfda917d7df.jpg"
+            alt="Whiskey Sour"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Gruyère, Bacon, and Spinach Scrambled Eggs</h1>
+        <div>
+          <ul>
+            <li>8 large eggs</li>
+            <li>1 tsp. Dijon mustard</li>
+            <li>Kosher salt and pepper</li>
+            <li>1 tbsp. olive oil or unsalted butter</li>
+            <li>2 slices thick-cut bacon, cooked and broken into pieces</li>
+            <li>2 c. spinach, torn</li>
+            <li>2 oz. Gruyère cheese, shredded</li>
+          </ul>
+          <p>
+            In a large bowl, whisk together eggs, Dijon mustard, 1 tablespoon
+            water and 1/2 teaspoon each salt and pepper. Heat oil or butter in
+            10-inch nonstick skillet on medium. Add eggs and cook, stirring with
+            rubber spatula every few seconds, to desired doneness, 2 to 3
+            minutes for medium-soft eggs. Fold in bacon, spinach, and Gruyère
+            cheese.
+          </p>
+          <img
+            src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/gruyere-bacon-and-spinach-scrambled-eggs-1608648723.jpg?crop=0.847xw:0.891xh;0.0969xw,0&resize=980:*"
+            alt="Gruyère, Bacon, and Spinach Scrambled Eggs"
+          />
+        </div>
+      </div>
+      <div className="recipe">
+        <h1>Raspberry Bakewell Cake</h1>
+        <div>
+          <ul>
+            <li>140g ground almond</li>
+            <li>140g butter, softened</li>
+            <li>140g golden caster sugar</li>
+            <li>140g self-raising flour</li>
+            <li>2 eggs</li>
+            <li>1 tsp vanilla extract</li>
+            <li>250g raspberry</li>
+            <li>2 tbsp flaked almond</li>
+            <li>icing sugar, to serve</li>
+          </ul>
+          <p>
+            Heat oven to 180C/160C fan/gas 4 and base-line and grease a deep
+            20cm loose-bottomed cake tin. Blitz the ground almonds, butter,
+            sugar, flour, eggs and vanilla extract in a food processor until
+            well combined. Spread half the mix over the cake tin and smooth over
+            the top. Scatter the raspberries over, then dollop the remaining
+            cake mixture on top and roughly spread – you might find this easier
+            to do with your fingers. Scatter with flaked almonds and bake for 50
+            mins until golden. Cool, remove from the tin and dust with icing
+            sugar to serve.
+          </p>
+          <img
+            src="https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-559459_11-4dd3009.jpg?quality=90&webp=true&resize=300,272"
+            alt="Raspberry Bakewell Cake"
+          />
+        </div>
+      </div>
     </div>
   );
 }
